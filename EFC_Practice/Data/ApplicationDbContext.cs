0ae498e46116a4b852de7da42c822e7c6f4c68a4
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-12 17:51:22 +1300

    Add ApplicationDbContext

--- EFC_Practice/Data/ApplicationDbContext.cs
+++ EFC_Practice/Data/ApplicationDbContext.cs
@@ -1,6 +1,16 @@
-﻿namespace EFC_Practice.Data
+﻿using EFC_Practice.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFC_Practice.Data
 {
-    public class ApplicationDbContext 
+    public class ApplicationDbContext : DbContext
     {
+
+        public ApplicationDbContext(ApplicationDbContext options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Customer> Customers { get; set; }
     }
 }
