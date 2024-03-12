8188358b02514b94dfc996e2bbf7a8fde57b7a27
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-12 18:54:29 +1300

    Add View Page for Customer List

---
+++ EFC_Practice/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using EFC_Practice.Data;
+using EFC_Practice.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFC_Practice.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CustomerController(ApplicationDbContext db)
+        {
+            _db = db;   
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Customer> customer = _db.Customers;
+            return View(customer);
+        }
+    }
+}
